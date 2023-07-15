b495410767f532b88ff49bb769f379ef09e36394
kokimoriguchi <skoki5231@icloud.com>
2023-07-15 17:32:07 +0900

    シフト作成APIの重複データの解消

--- backend/app/controllers/api/v1/employee_shifts_controller.rb
+++ backend/app/controllers/api/v1/employee_shifts_controller.rb
@@ -9,59 +9,30 @@ class Api::V1::EmployeeShiftsController < ApplicationController
     render json: {status: "success", data: @get_submit_month}
   end
 
-
-  #ShiftDataとShiftTimeを作成し、それらをトランザクションで保存する
-  #.save!とすることで保存に失敗した場合は例外を発生させる為、トランザクションでロールバックする
   def create
+    # Start transaction
     ActiveRecord::Base.transaction do
       params[:shiftDates].each do |shift|
         begin
-          # find_or_initialize_byメソッドで、既に存在する場合は取得、存在しない場合は新規作成する
-          @shift_date = ShiftDate.find_or_initialize_by(shift_date_params(shift))
-          @shift_date.is_attendance = true
-          @shift_date.save!
-        rescue ActiveRecord::RecordInvalid => e
-          render json: {status: "error", message: "ShiftDate error: #{e.message}"}
-          return
-        end
+          # EmployerShiftシフトレコードにemployee_idとwork_day（shift_dateテーブルを結合）が一致するレコードを取得
+          @employer_shift = EmployerShift.joins(:shift_date).find_by(employee_id: @current_employee_id, shift_dates: {work_day: shift_date_params(shift)[:work_day]})
 
-        begin
-          @shift_time = ShiftTime.new(shift_time_params(shift).merge(shift_date_id: @shift_date.id))
-          @shift_time.save!
-        rescue ActiveRecord::RecordInvalid => e
-          render json: {status: "error", message: "ShiftTime error: #{e.message}"}
-          return
-        end
-
-        begin
-          @employer_shifts = EmployerShift.new(shift_date_id: @shift_date.id, employee_id: @current_employee_id)
-          @employer_shifts.save!
-        rescue ActiveRecord::RecordInvalid => e
-          render json: {status: "error", message: "EmployerShift error: #{e.message}"}
-          return
-        end
-      end
-
-      # 月初の日付を取得
-      first_day = Date.parse(params[:shiftDates][0][:shift_date][:work_day]).beginning_of_month
-      last_day = first_day.end_of_month
-      # 月初から月末までの日付を作成
-      (first_day..last_day).each do |date|
-        begin
-          # 作成した日付がShiftDateに存在するか確認
-          unless ShiftDate.exists?(work_day: date)
-            # 存在しない場合は、is_attendanceをfalseで作成
-            ShiftDate.create!(work_day: date, is_attendance: false)
+          # EmployerShiftテーブルにレコードが存在しない場合は、ShiftDateとShiftTimeを作成し、EmployerShiftを作成する
+          if @employer_shift.nil?
+            @shift_date = ShiftDate.create!(shift_date_params(shift).merge(is_attendance: true))
+            @shift_time = ShiftTime.create!(shift_time_params(shift).merge(shift_date_id: @shift_date.id))
+            @employer_shift = EmployerShift.create!(employee_id: @current_employee_id, shift_date_id: @shift_date.id)
+          else
+            # EmployerShiftテーブルにレコードが存在する場合は、ShiftDateとShiftTimeを更新する
+            @shift_time = ShiftTime.find_by(shift_date_id: @employer_shift.shift_date_id)
+            @shift_time.update!(shift_time_params(shift))
           end
         rescue ActiveRecord::RecordInvalid => e
-          render json: {status: "error", message: "ShiftDate error: #{e.message}"}
+          render json: {status: "error", message: e.message}
           return
         end
       end
-
       render json: {status: "create", data: {shift_date: params[:shiftDates], shift_time: params[:shiftTimes]}}
-    rescue ActiveRecord::RecordInvalid => e
-      render json: {status: "error", message: e.message}
     end
   end
 
