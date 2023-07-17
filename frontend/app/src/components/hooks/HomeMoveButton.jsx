8786f0e429284cf28aad4ffbb1032a77ead00ea2
kokimoriguchi <skoki5231@icloud.com>
2023-07-17 11:53:22 +0900

    Homeのボタン編集

---
+++ frontend/app/src/components/hooks/HomeMoveButton.jsx
@@ -0,0 +1,18 @@
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+export const HomeMoveButton = ({ onClick, children }) => {
+  return (
+    <div
+      className=" relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium transition duration-300 ease-out border-2 border-sky-800 rounded-full shadow-md group cursor-pointer w-64"
+      onClick={onClick}
+    >
+      <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-sky-800 group-hover:translate-x-0 ease">
+        <HiOutlineArrowRight className="font-bold text-2xl" />
+      </span>
+      <span className="font-mono font-extrabold absolute flex items-center justify-center w-full h-full dark:text-white text-sky-800 transition-all duration-300 transform group-hover:translate-x-full ease">
+        {children}
+      </span>
+      <span className="relative invisible">{children}</span>
+    </div>
+  );
+};
--- frontend/app/src/components/pages/Home.jsx
+++ frontend/app/src/components/pages/Home.jsx
@@ -1,52 +1,47 @@
 import { FadeIn } from "../hooks/FadeInHook";
 import { useNavigate } from "react-router-dom";
+import { HomeMoveButton } from "../hooks/HomeMoveButton";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="text-center pt-20 dark:bg-black dark:text-white">
+    <div className="text-center pt-20 pb-20 dark:bg-black dark:text-white">
       <FadeIn delay={0}>
-        <h1 className="text-3xl font-extrabold">Home</h1>
+        <h1 className="text-3xl font-extrabold font-mono">Home</h1>
       </FadeIn>
       <FadeIn delay={100}>
-        <h2 className="text-2xl font-bold">
+        <h2 className="text-2xl font-bold font-mono">
           Welcome to the Store Management System
         </h2>
         <p className="m-auto animate-bounce w-6 h-6 pt-5">⬇︎</p>
       </FadeIn>
       <div className="pt-8">
-        <FadeIn delay={200}>
-          <button
-            className="text-xl font-bold hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out pb-3"
-            onClick={() => navigate("login")}
-          >
-            Please login to continue
-          </button>
+        <FadeIn delay={300}>
+          <div className="pb-5">
+            <HomeMoveButton onClick={() => navigate("login")}>
+              スタッフログイン
+            </HomeMoveButton>
+          </div>
+        </FadeIn>
+        <FadeIn delay={600}>
+          <div className="pb-5">
+            <HomeMoveButton onClick={() => navigate("/manager/login")}>
+              マネージャーログイン
+            </HomeMoveButton>
+          </div>
         </FadeIn>
         <FadeIn delay={900}>
-          <button
-            className="text-xl font-bold hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out pb-3"
-            onClick={() => navigate("/manager/login")}
-          >
-            Or login as a manager
-          </button>
+          <div className="pb-5">
+            <HomeMoveButton onClick={() => navigate("store/create")}>
+              新規店舗登録
+            </HomeMoveButton>
+          </div>
         </FadeIn>
         <FadeIn delay={1200}>
-          <button
-            className="text-xl font-bold hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out pb-3"
-            onClick={() => navigate("store/create")}
-          >
-            Or create a new store
-          </button>
-        </FadeIn>
-        <FadeIn delay={1500}>
-          <button
-            className="text-xl font-bold hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out pb-3"
-            onClick={() => navigate("/employee/create")}
-          >
-            Or create a new staff
-          </button>
+          <HomeMoveButton onClick={() => navigate("/employee/create")}>
+            新規スタッフ登録
+          </HomeMoveButton>
         </FadeIn>
       </div>
     </div>
