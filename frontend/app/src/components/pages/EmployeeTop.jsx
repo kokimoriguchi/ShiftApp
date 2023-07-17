cf0a519116a7e717e2b4db14d4bea587f6f56189
kokimoriguchi <skoki5231@icloud.com>
2023-07-17 15:30:50 +0900

    スタッフトップ画面仮完成

--- frontend/app/src/components/pages/EmployeeTop.jsx
+++ frontend/app/src/components/pages/EmployeeTop.jsx
@@ -1,6 +1,7 @@
 import baseAxios from "../hooks/Axios";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { HomeMoveButton } from "../hooks/HomeMoveButton";
 
 const EmployeeTop = () => {
   const navigate = useNavigate();
@@ -15,12 +16,18 @@ const EmployeeTop = () => {
     }
   };
   return (
-    <div className="text-center flex flex-col pt-40">
-      <h1>StaffTop</h1>
-      <button onClick={handleCheckUser}>ユーザー情報取得</button>
-      <button onClick={() => navigate(`/staff/${storeNumber}/calender/submit`)}>
-        submitShift
-      </button>
+    <div className="flex flex-col items-center dark:bg-black dark:text-white h-screen">
+      <h1 className="font-mono font-extrabold pt-20">StaffTop</h1>
+      <div className="pb-5 pt-10">
+        <HomeMoveButton onClick={handleCheckUser}>ユーザー確認</HomeMoveButton>
+      </div>
+      <div>
+        <HomeMoveButton
+          onClick={() => navigate(`/staff/${storeNumber}/calender/submit`)}
+        >
+          シフト提出
+        </HomeMoveButton>
+      </div>
     </div>
   );
 };
