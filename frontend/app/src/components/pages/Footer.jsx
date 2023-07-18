c16ab0d65aa864dded914c9ee5f94a9f45303705
kokimoriguchi <skoki5231@icloud.com>
2023-07-18 18:41:42 +0900

    footer作成

---
+++ frontend/app/src/components/pages/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="max-h-fit dark:bg-black">
+      <div className="mb-5 w-full h-0.5 dark:bg-black bg-gray-500 z-[-1]"></div>
+    </div>
+  );
+};
+
+export default Footer;
