bc02438eb73a5c890851856c20d499796e881dd6
kokimoriguchi <skoki5231@icloud.com>
2023-07-25 11:03:02 +0900

    ECSデプロイ途中

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost'
+    origins 'http://localhost', 'https://realworld-demo.com'
 
     resource '*',
       headers: :any,
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# # データベースの作成、マイグレーション、シードデータの投入。createとseedはfargateの初回のみ実行する。
+# bundle exec rails db:create
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
