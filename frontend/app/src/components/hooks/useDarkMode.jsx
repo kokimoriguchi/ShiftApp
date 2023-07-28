521bf5f9a73bc79c17df35b80787e0e692b392f8
kokimoriguchi <skoki5231@icloud.com>
2023-07-28 10:50:07 +0900

    darkmodeをローカルストレージで保持する様変更

--- frontend/app/src/components/hooks/useDarkMode.jsx
+++ frontend/app/src/components/hooks/useDarkMode.jsx
@@ -1,18 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
 
 // dark mode と light mode を切り替える
-export const useDarkMode = (isInitialDark = false) => {
-  const [isDarkMode, toggleTheme] = useState(isInitialDark);
-  const toggle = useCallback((isDark) => {
-    if (typeof isDark === "undefined") {
-      toggleTheme((state) => !state);
-      return;
-    }
+export const useDarkMode = () => {
+  // ローカルストレージから初期状態を取得する
+  const initialMode = window.localStorage.getItem("darkMode") === "true";
 
-    toggleTheme(isDark);
+  const [isDarkMode, setIsDarkMode] = useState(initialMode);
+
+  const toggle = useCallback(() => {
+    setIsDarkMode((prevMode) => !prevMode);
   }, []);
 
   useEffect(() => {
+    // ダークモードの状態をローカルストレージに保存する
+    window.localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
+
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
     } else {
