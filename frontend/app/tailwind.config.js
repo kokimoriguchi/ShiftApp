f978635bfb12510da310dc855984fb833132ccca
kokimoriguchi <skoki5231@icloud.com>
2023-07-29 22:35:16 +0900

    heartbeatクラスの追加

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       animation: {
         "tracking-in-expand":
           "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000)   both",
+        heartbeat: "heartbeat 1.5s ease  infinite both",
       },
       keyframes: {
         "tracking-in-expand": {
@@ -19,6 +20,29 @@ module.exports = {
           },
           to: {
             opacity: "1",
+          },
+        },
+        heartbeat: {
+          "0%": {
+            transform: "scale(1)",
+            "transform-origin": "center center",
+            "animation-timing-function": "ease-out",
+          },
+          "10%": {
+            transform: "scale(.91)",
+            "animation-timing-function": "ease-in",
+          },
+          "17%": {
+            transform: "scale(.98)",
+            "animation-timing-function": "ease-out",
+          },
+          "33%": {
+            transform: "scale(.87)",
+            "animation-timing-function": "ease-in",
+          },
+          "45%": {
+            transform: "scale(1)",
+            "animation-timing-function": "ease-out",
           },
         },
       },
