8665fa07e8135b6679fa49c7ae9a628ef443b46b
kokimoriguchi <skoki5231@icloud.com>
2023-07-29 10:38:15 +0900

    footer cp追記

--- frontend/app/src/components/pages/Footer.jsx
+++ frontend/app/src/components/pages/Footer.jsx
@@ -2,6 +2,12 @@ const Footer = () => {
   return (
     <div className="h-auto">
       <div className="mb-5 w-full h-0.5 dark:bg-white bg-gray-500 z-[-1]"></div>
+      <div className=" text-xs opacity-70">
+        <div className="flex justify-center">
+          <p className="pr-1">Copyright</p>
+          <p className="flex justify-center">&copy; 2023 km</p>
+        </div>
+      </div>
     </div>
   );
 };
