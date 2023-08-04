b9dcf2471bfbe5405175ab2353611f17bc4f825f
kokimoriguchi <skoki5231@icloud.com>
2023-08-04 11:29:42 +0900

    secretkeyについて追記

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
